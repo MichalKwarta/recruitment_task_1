9f800e3eeb986527941138c0a8a7f1873ab95d64
MichalKwarta <Michu1164@gmail.com>
2022-04-14 18:12:24 +0200

    mdn tutorial to deployment

--- book_list/settings.py
+++ book_list/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LANGUAGE_CODE = "pl-PL"
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
