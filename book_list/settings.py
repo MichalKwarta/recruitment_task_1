c4c21df7d2dce19e40cf34436f3f6822acd46926
MichalKwarta <Michu1164@gmail.com>
2022-04-14 17:41:14 +0200

    debug true

--- book_list/settings.py
+++ book_list/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h&(e4=i#ptj&d)1lwn9tu7@1tnk$j#_3^hau7f0xh38sq)_qf('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS =['*'] 
 
